2a45e1dce903fc0db59c4c1c9470d9d0fd754cdf
Daniel L <theusafyt@gmail.com>
2017-10-31 22:01:23 +0000

    Drawephone update

--- Drawephone/main.js
+++ Drawephone/main.js
@@ -24,11 +24,16 @@ function start(){
     var userNameInput = document.getElementById("user");
     var serverInput = document.getElementById("server");
     
+    //start data
+    var fi = document.getElementById("fi");
+    var sbu = document.getElementById("start");
+    var sinfo = document.getElementById("theme");
+    
     //connects to game?
     function loadGame(int){
-        awayD1 = ("Game" + int);
-        var game = rootData.child("Game" + int);
-        var pD = "";
+        awayD1 = ("Game" + int); //idk
+        var game = rootData.child("Game" + int); //shows game ur in
+        var pD = ""; //stores player data
         game.once("value",function(s){
             if(s.val().Misc.Started === false){
                 pD = s.val().Players;
@@ -37,6 +42,7 @@ function start(){
                     pD = "";
                     n = 0;
                     game.child("Players").set(gameName);
+                    fi.style.display = "block";
                 }else{
                     //WIP
                     game.child("Players").set(pD + "," + gameName);
@@ -44,27 +50,37 @@ function start(){
                 }
             }
         });
+        var plD = ""; //stores active player data
         game.child("Players").on("value",function(t){
             setTimeout(function(){ //Prevent player "None" from appearing in game?
-                var plD = t.val();
+                plD = t.val();
                 var plDli = plD.split(",");
                 var tab = document.getElementById("list");
                 var nu = plD.split(",").length;
                 tab.innerHTML = "";
                 for(var i = 0; i < nu; i++){
-                    tab.innerHTML += "<td>" + (i+1) + ". " + plDli[i] + "</td>\n";
+                    tab.innerHTML += "<td style='display: block'>" + (i+1) + ". " + plDli[i] + "</td>\n";
                 }
             },1000);
         });
+        sbu.onmousedown = function(){
+            game.child("Misc").child("Theme").set(sinfo.value);
+            if(plD.split(",").length >= 3){
+                game.child("Misc").child("Started").set(true);
+            }else{
+                alert("There are not enough players online. Press the button again to change the Theme or start the game");
+            }
+        }
     }
     
     //setup
-    var n = 0;
+    var n = 0; //your player id.
     var gameID;
     var inGame = false;
     var gameName;
+    
     submitButton.onclick = function(){
-        if(inGame === false && userNameInput.value !== "" && userNameInput.value.search(",") === -1){
+        if(inGame === false && serverInput.value !== "Choose" && userNameInput.value !== "" && userNameInput.value !== "none" && userNameInput.value.search(",") === -1 && userNameInput.value.length < 20){
             inGame = true;
             gameID = serverInput.value;
             gameName = userNameInput.value;
@@ -75,11 +91,14 @@ function start(){
             }else if(gameID === "Game3"){
                 loadGame(3);
             }
+        }else{
+            alert("Your username is not valid, or something is broken. Try with a new username, or reload the page");
         }
     };
     
     //read only data
     var awayD1 = -1;
+    
     
     //remove player on unload. currently removes all players, causing loads of issues :p
     window.onbeforeunload = function(){
