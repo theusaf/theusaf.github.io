f0a6e4d6b1817679ec3590dd51cf3e0a6a7d61fc
Daniel L <theusafyt@gmail.com>
2018-08-06 19:12:03 +0000

    V 1.6.0.0 - Added testfor

--- MCRecipe/index.js
+++ MCRecipe/index.js
@@ -1,5 +1,5 @@
 //MC COMMAND UPDATER
-//VERSION 1.5.13.0
+//VERSION 1.6.0.0
 //(Major Update).(Commands_Completed).(Misc Changes).(if applicable, this is actually higher than the previous number and means that a subcommand has been completed)
 /*Update log:
 *Some Dates may be off
@@ -54,6 +54,7 @@ Sunday 08/05/2018
 1.5.13 Adding more damage values...
 Monday 08/06/2018
 1.5.14 Adding scoreboard command support
+1.6.0 Finished testfor kindof
 */
 
 /*Notes and Random Comments
@@ -315,8 +316,16 @@ function parse(typ,version,version2,te){
             })();
             break;
         case 'testfor':
-            //try to find a command that will run for all players without destroying gameplay
-            //will be done after summon is complete. follows following format: /execute if entity <entity> run 
+            (function(){
+            var ar = te.split(" ");
+            if(ar.length > 3){
+                ar[2] = ar.slice(2).join(" ");
+            }
+            ar[1] = checkSelector(ar[1].substr(2));
+            ar[1] = ar[1].substr(0,ar[1].length - 1) + ",nbt=" + ar[2] + "]"
+            //checkEntityNBT later :p
+            fin = "execute if entity " + ar[1] + "run summon falling_block ~ ~ ~ {BlockState:{Name:\"barrier\"}}";
+            });
             break;
         case 'summon':
             var ar = te.split(" ");
@@ -419,9 +428,11 @@ function parse(typ,version,version2,te){
             })();
             break;
         case 'help':
+            (function(){
             var randomHelpList = ["try fixing a real command...","Subscribe! https://www.youtube.com/theusaf","Type /help for a list of commands","This is a line of text","This might help you! https://shortr.github.io/?A","Dolphins are cool!","I wish we could ride dolphins","Stop pressing the fix button please...","Error 404-Command found","Open the banana from the bottom!","Those are my cookies","Searge says theusaf says Dinnerbone says hi","https://www.minecraft.net","Not an official Minecraft website... Duh...","Have you tried swimming in the ocean?"];
             var randomStr = randomHelpList[Math.round(Math.random()*randomHelpList.length - 1)];
             fin = "theusaf says " + randomStr;
+            });
             break;
         default:
             console.error("parseError: Unidentified type " + typ + " or invalid version");
